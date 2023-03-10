d1c62c5a23058ef1f0bea47e9e3e50930c63680d
Dzmitry-yarik <107866389+Dzmitry-yarik@users.noreply.github.com>
2023-03-10 15:16:29 +0300

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+          int a = 1;
+        int b = 1;
+        int sum_fib;
+        System.out.println("1\n1");
+        for(int i = 0; i < lastFibonacci; i++){
+            sum_fib = a + b;
+            a = b;
+            b = sum_fib;
+            System.out.println(sum_fib);
+        }
     }
 }
